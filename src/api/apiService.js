611de7b0981e2da0cf5bb159ad51f75cdf4e3fe5
Dvooskid <dvooskid1234@gmail.com>
2025-08-02 12:27:44 +0100

    Update apiService.js

--- src/api/apiService.js
+++ src/api/apiService.js
@@ -3,7 +3,7 @@ import { jwtDecode } from "jwt-decode";
 import { toast } from "react-toastify";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "https://thrive-intranet-hr-backend.onrender.com/api",
+  baseURL: process.env.REACT_APP_API_URL || "https://dvooskid.pythonanywhere.com/api",
   timeout: 10000,
 });
 
